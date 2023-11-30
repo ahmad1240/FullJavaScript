63d7bf6d2d45eaf606b1daf11febb2c08ec61e65
Mohammad Ahmad <mohammadahmad4@gmail.com>
2023-11-30 12:20:10 +0530

    folder change

---
+++ basics/conversion_operation.js
@@ -0,0 +1,27 @@
+// let score = 33;
+// console.log(typeof score);
+// let score1 = "33";
+// console.log(typeof score1);
+
+// let valueInNumber = Number(score1);
+
+// console.log(typeof valueInNumber);
+
+// "33"=>33
+// "33abc"=> Nan but type number hai
+// true=> 1
+// false=> 0
+
+let isLoggedIn = 1;
+let booleanValue = Boolean(isLoggedIn);
+console.log(booleanValue);
+// 1=> true
+// 0=> false
+
+//""=>false
+//"ahmad"=>true
+
+let stringNu = 33;
+let stringValue = String(stringNu);
+console.log(stringValue);
+console.log(typeof stringValue);
