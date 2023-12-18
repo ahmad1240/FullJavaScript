77aad4d05ec6262a8ad9622b33ec36c098a8a6d6
Mohammad Ahmad <mohammadahmad4@gmail.com>
2023-12-18 19:37:10 +0530

    BMI

---
+++ projects/BmiCalculator/script.js
@@ -0,0 +1,24 @@
+const from = document.querySelector("form");
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+from.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please provide a valid height ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    if (bmi < 18.6) results.innerHTML = `Under weight <span>${bmi}</span> `;
+    else if (bmi >= 18.6 && bmi < 24.9)
+      results.innerHTML = `Normal weight <span>${bmi}</span> 
+    `;
+    else results.innerHTML = `Over weight <span>${bmi}</span> `;
+    // //show the result
+    // results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
